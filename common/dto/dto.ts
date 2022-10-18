f29fa346c203776db5dcc4339ad1d455f1fcaf79
Timur Seitosmanov <timur.seitosmanov@infrabel.be>
2022-10-18 11:58:19 +0200

    refactoring example.

--- common/dto/dto.ts
+++ common/dto/dto.ts
@@ -1,31 +1,35 @@
-import {MessageType, ModeratorMessageType, TypeWSMessage} from "./types";
+import { MessageType, ModeratorMessageType, TypeWSMessage } from "./types";
+
+export type LikeMessage = { count: number; messageId: string };
+export type ConfirmedMessage = { isConfirmed: true, messageId: string };
+export type RemoveMessage = { messageId: string }
 
 export type WsMessage =
     | {
-    type: TypeWSMessage.CONNECT;
-    data: MessageType[];
-}
+        type: TypeWSMessage.CONNECT;
+        data: MessageType[];
+    }
     | {
-    type: TypeWSMessage.MESSAGE;
-    data: MessageType;
-}
+        type: TypeWSMessage.MESSAGE;
+        data: MessageType;
+    }
     | {
-    type: TypeWSMessage.LIKES;
-    data: { count: number; messageId: string };
-}
+        type: TypeWSMessage.LIKES;
+        data: LikeMessage;
+    }
     | {
-    type: TypeWSMessage.CONFIRMED_MESSAGE
-    data: { isConfirmed: true, messageId: string }
-}
-| {
-    type: TypeWSMessage.REPLY_TO_MESSAGE
-    data: ModeratorMessageType
-}
-| {
-    type: TypeWSMessage.REMOVE_MESSAGE
-    data: {messageId: string}
-}
-| {
-    type: TypeWSMessage.GET_MESSAGES,
-    data: MessageType[]
-}
+        type: TypeWSMessage.CONFIRMED_MESSAGE
+        data: ConfirmedMessage;
+    }
+    | {
+        type: TypeWSMessage.REPLY_TO_MESSAGE
+        data: ModeratorMessageType
+    }
+    | {
+        type: TypeWSMessage.REMOVE_MESSAGE
+        data: RemoveMessage;
+    }
+    | {
+        type: TypeWSMessage.GET_MESSAGES,
+        data: MessageType[]
+    }
